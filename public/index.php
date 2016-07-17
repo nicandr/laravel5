80555f1048c6f743ae9e211df94e71a4e280ebe0
nicandr <andr.nicolai1@gmail.com>
2016-07-17 22:02:00 +0300

    commit second

--- public/index.php
+++ public/index.php
@@ -38,7 +38,7 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 |--------------------------------------------------------------------------
 | Run The Application
 |--------------------------------------------------------------------------
-|222223333
+|222223333pp
 | Once we have the application, we can handle the incoming request
 | through the kernel, and send the associated response back to
 | the client's browser allowing them to enjoy the creative
